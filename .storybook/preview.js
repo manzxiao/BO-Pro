5d54daed3f1e2c5fc200582130786807c738745d
manzxiao <manzxiao@xm.remarkablefoods.com>
2021-05-12 14:47:13 +0800

    init npm

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,4 +2,10 @@
 export const parameters = {
   // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
   actions: { argTypesRegex: '^on.*' },
+  a11y: {
+    // the target DOM element
+    element: '#root',
+    // sets the execution mode for the addon
+    manual: false,
+  },
 };
